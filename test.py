ef76ebce2bd18f7ba7c8ec3215a930a5bcfdb63b
Nat Barshay <nbarshay@Nats-Mac-Studio.local>
2024-10-19 17:38:00 -0400

    makeAnimation basically works

--- test.py
+++ test.py
@@ -3,26 +3,34 @@ import numpy as np
 from pylab import *
 import numpy as np
 
-pixels = 24
 
-def doit():
-   return np.random.rand(pixels, pixels)/0.5
+def doit(frame, n_frames, n_pixels):
+   return 0.0 + np.random.rand(n_pixels, n_pixels)*0.1 + float(frame)/n_frames*0.2
+
+"""
+- Filetype should be 'mp4'
+- Always square
+- Always greyscale
+- Filetype should be mp4
+"""
+def makeAnimation(path, get_frame, n_frames, n_pixels):
+    fig = plt.figure(figsize=(1, 1))
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    im = ax.imshow(np.zeros((n_pixels, n_pixels)), cmap="gray", vmin=0.0, vmax=1.0)
 
 
-fig = plt.figure(figsize=(1, 1))
-ax = plt.Axes(fig, [0., 0., 1., 1.])
-ax.set_axis_off()
-fig.add_axes(ax)
-im = ax.imshow(doit(), cmap="gray")
+    def update_img(n):
+        im.set_data(1.0 - get_frame(n, n_frames, n_pixels) )
+        return im
+
+    ani = animation.FuncAnimation(fig,update_img,300,interval=40)
+    writer = animation.writers['ffmpeg'](fps=25)
+
+    ani.save(path, writer=writer,dpi=n_pixels)
 
 
-def update_img(n):
-    im.set_data(doit())
-    return im
-
-ani = animation.FuncAnimation(fig,update_img,300,interval=40)
-writer = animation.writers['ffmpeg'](fps=25)
-
-ani.save('demo.mp4',writer=writer,dpi=pixels)
+makeAnimation('test.mp4', doit, 400, 512)
 
 
