1b8928c4f72bb176a46036a3ae55194d39e51c89
Nat Barshay <nbarshay@Nats-Mac-Studio.local>
2024-10-24 17:16:01 -0400

    basic fft

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
     - venv lives in env folder of repo
         - We run: `pip freeze > requirements.txt` 
 
+
+# Ipython
+- https://stackoverflow.com/questions/35037815/how-to-automatically-reload-modules-in-ipython#:~:text=1%20Answer,Done.
+- we use autoreload 1, thus %aimport
+
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import matplotlib.animation as animation
 import numpy as np
-from pylab import *
+from pylab import plt
 import numpy as np
+from math import cos, pi, sqrt, exp
+import random
+import matplotlib.pyplot as plt
 
 
 def doit(frame, n_frames, n_pixels):
@@ -36,6 +39,56 @@ def makeAnimation(path, get_frame, n_frames, n_pixels):
 #------------------------
 
 
+def selectRandomAmongOptions(values):
+    values = np.array(values).astype(np.float64)
+    assert np.all(values >= 0.0)
+    assert values.ndim == 1
+    tot = np.sum(values)
+    p = values/tot if tot > 0.0 else np.ones(len(values)/len(values))
+    return np.random.choice(len(values),  p=p)
+
+
+
+
+
+locs = [ np.array([0.5, 0.5]), np.array([0.6, 0.6]) ]
+
+n_loops = 16
+
+for loop in range(n_loops):
+    for i in range(8):
+        x = ((i+1)%8)/8
+        raw_amp = cos(2*pi*x) + cos(4*pi*x)  + cos(8*pi*x) + cos(16*pi*x) 
+        #if i+1 == 0:
+        #    raw_amp -= 2.0
+        #elif i+1==2 or i+1==6:
+        #    raw_amp += 3.0
+
+        options = []
+
+        for flip in [False, True]:
+            amp = (raw_amp + 1/sqrt(2) + 0.5) / (4 + 1/sqrt(2) + 0.5)
+            theta = (pi - 0.1) * amp * (-1.0 if flip else 1.0)
+
+            c, s = np.cos(theta), np.sin(theta)
+            rot = np.array(((c, -s), (s, c)))
+
+            head, tail = locs[-2], locs[-1]
+
+            options.append( rot.dot(tail - head) + tail)
+
+        def getScore(new_loc):
+            dist = np.linalg.norm(new_loc - np.array([0.5, 0.5]))
+            score = exp(-dist)
+            return score**4
+
+
+
+        scores = list(map(getScore, options))
+        idx = selectRandomAmongOptions(scores)
+        #idx = np.argmax(scores)
+
+        locs.append(options[idx])
 
 
 
@@ -46,14 +99,15 @@ def doit2(frame, n_frames, n_pixels):
 
     def drawPt(vec, x_off=0, y_off=0, weight=1.0):
             def getLoc(raw, off):
+                    raw = (raw%1.0 + 1.0)%1.0
                     return max(min(int(raw*n_pixels)+off, n_pixels-1), 0)
             
             img[getLoc(vec[0], x_off), getLoc(vec[1], y_off)] += weight
 
 
     def drawDot(loc):
-        for x_off in range(-2, 3):
-            for y_off in range(-2, 3):
+        for x_off in range(-1, 2):
+            for y_off in range(-1, 2):
                 drawPt(loc, x_off, y_off)
 
 
@@ -69,7 +123,8 @@ def doit2(frame, n_frames, n_pixels):
 
     loop_frames = n_frames
     
-    segments = np.array([[0.5, 0.9], [0.40, 0.5], [0.5, 0.1], [0.6, 0.5]]*4)
+    #segments = np.array([[0.5, 0.9], [0.40, 0.5], [0.5, 0.1], [0.6, 0.5]]*4)
+    segments = np.array(locs)
 
     distances = np.sqrt(np.sum((segments[1:,:] - segments[:-1,:])**2, axis=1))
     total_distance = np.sum(distances)
@@ -81,14 +136,14 @@ def doit2(frame, n_frames, n_pixels):
 
         frame_center = distance_acc / total_distance * n_frames
 
-        alpha = 4 - abs(frame - frame_center) 
+        alpha = (16 - abs(frame - frame_center))/16.0
 
         if i > 0 and alpha >= 0:
             a, b, c = segments[i-1], segments[i], segments[i+1]
             direc = (b-a) / np.linalg.norm(b-a) + (c-b) / np.linalg.norm(c-b)
             direc /= np.linalg.norm(direc)
-            drawLine(b, b+direc*0.02, 10, alpha)
-            drawLine(b, b-direc*0.02, 10, alpha)
+            drawLine(b, b+direc*0.01, 10, alpha)
+            drawLine(b, b-direc*0.01, 10, alpha)
 
 
         distance_acc += next_dist
@@ -115,12 +170,55 @@ def doit2(frame, n_frames, n_pixels):
 
     return img
 
-bpm = 85
 
-
-makeAnimation('test.mp4', doit2, (25*60*2*4)//85, 512)
+def doCurrent():
+    #hack 4x frames
+    makeAnimation('test.mp4', doit2, (25*60*4*n_loops)//81, 512)
     
 
 
 
+def specGram(rate, data):
+    from scipy import fft
+
+    assert rate == 44100
+
+
+    # Spectrogram estimation:
+    N = 256
+    S = []
+    T = []
+    T2 = []
+    for k in range(0, data.shape[0]+1, N):
+        x = fft.fftshift(fft.fft(data[k:k+N,0], n=N))[N//2:N]
+        # assert np.allclose(np.imag(x*np.conj(x)), 0)
+        raw_px = np.real(x*np.conj(x))
+        Pxx = 10*np.log10(raw_px)
+        S.append(Pxx)
+        T.append(10*np.log10(np.sum(raw_px[35:70])))
+        T2.append(10*np.log10(np.sum(raw_px[:5])))
+    S = np.array(S)
+
+    # Frequencies:
+    f = fft.fftshift(fft.fftfreq(N, d=1/rate))[N//2:N]
+    # array([    0. ,   187.5,   375. , ..., 23625. , 23812.5])
+
+    # Spectrogram rendering:
+    n_beats = (data.shape[0] / rate) * (64 / 24) 
+
+    fig, axs = plt.subplots(3)
+
+    axs[0].imshow(S.T, origin='lower', extent = [0, n_beats, 0, S.shape[1]], aspect='auto')
+    
+
+    axs[1].plot(np.linspace(0, n_beats, len(T)), T)
+    axs[1].margins(x=0, y=0)
+
+    axs[2].plot(np.linspace(0, n_beats, len(T2)), T2)
+    axs[2].margins(x=0, y=0)
+
+
+
+
+    plt.show()
 
